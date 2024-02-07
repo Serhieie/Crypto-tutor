746f479d5f6e1d38a4d1673e875945f7fe037527
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-07 18:32:04 +0200

    bundle: 404

--- src/NoPage.tsx
+++ src/NoPage.tsx
@@ -1,13 +1,20 @@
 export const NoPage: React.FC = () => {
   return (
-    <div className="select-none mt-0 h-[92vh] flex flex-col items-center justify-center">
-      <div className="relative bg-transparent bg-opacity-20 p-14  rounded-[60px] backdrop-filter backdrop-blur-sm">
+    <div
+      className="select-none mt-0 h-[100vh-64px] flex 
+    flex-col items-center justify-center"
+    >
+      <div
+        className="relative bg-transparent bg-opacity-20 p-14 pt-4 
+       rounded-[60px] backdrop-filter backdrop-blur-sm"
+      >
         <h1
-          className={`"text-blue-300 font-errorFont md:text-[120px] text-[200px] text-center select-none font-extrabold p-0 mt-10`}
+          className={` text-slate-600 font-montserrat md:text-[120px] 
+          text-[200px] text-center select-none font-extrabold p-0 mt-0`}
         >
           404
         </h1>
-        <p className={` "text-blue-300 text-lg font-pixel text-center mt-20 px-2`}>
+        <p className={` text-slate-600 text-lg font-pixel text-center mt-0 px-2`}>
           Page not found
         </p>
       </div>
