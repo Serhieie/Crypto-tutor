f3a5a4a514cb8183df9a2af0ef579966aedb7a8e
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-07 20:41:21 +0200

    fix: hashRouts

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
     <React.StrictMode>
       <Provider store={store}>
-        <HashRouter basename="Crypto-tutor">
+        <HashRouter>
           {/* <BrowserRouter basename="/Crypto-tutor"> */}
           <PersistGate loading={null} persistor={persistor}>
             <App />
