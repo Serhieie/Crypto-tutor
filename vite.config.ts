4896911a71281ebffbe13de6879fbecae23504c4
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-19 13:27:26 +0200

    fix:08

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: { watch: { usePolling: true } },
   plugins: [react()],
-  base: process.env.NODE_ENV === "production" ? "/сrypto-helper/" : "/",
+  base: "/сrypto-helper/",
   build: {
     outDir: "dist",
     assetsDir: ".",
