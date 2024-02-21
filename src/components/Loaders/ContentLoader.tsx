215843e7a3f6f4a8f65e10ebb87b84d3d31116e7
Serhieie <89160714+Serhieie@users.noreply.github.com>
2024-02-21 17:16:18 +0200

    styles: loader to middle

--- src/components/Loaders/ContentLoader.tsx
+++ src/components/Loaders/ContentLoader.tsx
@@ -7,14 +7,16 @@ export const ContentLoader: React.FC = () => {
   return (
     <div
       className={`
-        ${isCoinShowed && assets.length > 0 ? " hidden " : " w-[75%] "}
-      ${!assets.length ? " w-[100%] " : "  w-[75%]  lg:w-[100%] "}
-      text-center   h-full min-h-[calc(100vh-86px)]
-       text-sky-200 bg-[#1E293B] p-4 transition-all rounded-2xl overflow-hidden `}
+    ${isCoinShowed && assets.length > 0 ? " hidden " : " w-[75%] "}
+    ${!assets.length ? " w-[100%] " : "  w-[75%]  lg:w-[100%] "}
+    text-center   h-full min-h-[calc(100vh-86px)]
+    text-sky-200 bg-[#1E293B] p-4 transition-all rounded-2xl overflow-hidden 
+    relative  
+  `}
     >
       <div
-        className="flex items-center justify-center 
-        h-full w-[100%] mt-[26%]"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        style={{ zIndex: 999 }}
       >
         <Spin size="large" />
       </div>
--- src/components/Loaders/SuspenseLoader.tsx
+++ src/components/Loaders/SuspenseLoader.tsx
@@ -4,12 +4,14 @@ export const SuspenseLoader: React.FC = () => {
   return (
     <div
       className={`
-      text-center    h-full min-h-[calc(100vh-86px)]
-       text-sky-200 bg-transparent w-full p-4 transition-all rounded-2xl overflow-hidden `}
+    text-center    h-full min-h-[calc(100vh-86px)]
+    text-sky-200 bg-transparent w-full p-4 transition-all rounded-2xl overflow-hidden 
+    relative 
+  `}
     >
       <div
-        className="flex   items-center justify-center 
-        h-full w-[100%] mt-[26%]"
+        className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+        style={{ zIndex: 999 }}
       >
         <Spin size="large" />
       </div>
